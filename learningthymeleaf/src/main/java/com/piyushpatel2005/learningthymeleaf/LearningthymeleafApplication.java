aea53953c6338ed7e22210879d20b26cb9557180
Piyush Patel <piyushpatel2005@gmail.com>
2018-04-29 00:27:02 -0400

    Initialize thymeleaf project

---
+++ learningthymeleaf/src/main/java/com/piyushpatel2005/learningthymeleaf/LearningthymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.piyushpatel2005.learningthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningthymeleafApplication.class, args);
+	}
+}
