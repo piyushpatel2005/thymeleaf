2eef0883ef56431cca30da774697ad153d1fdab3
Piyush Patel <piyushpatel2005@gmail.com>
2018-04-30 22:59:28 -0400

    Validation of form

--- learningthymeleaf/src/main/java/com/piyushpatel2005/learningthymeleaf/MainController.java
+++ learningthymeleaf/src/main/java/com/piyushpatel2005/learningthymeleaf/MainController.java
@@ -5,6 +5,7 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javax.validation.Valid;
 
@@ -19,40 +20,43 @@ public class MainController {
 
 	@RequestMapping("/")
 	public ModelAndView homePage() {
-		Map<String, Object> model = new HashMap<>();
-		model.put("username", "Matt Patel");
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("username", "Matt Greencroft");
 		model.put("id", 173);
 		return new ModelAndView("homepage", "model", model);
 	}
 	
 	@RequestMapping("/profile")
 	public ModelAndView viewProfile() {
-		Map<String, Object> model = new HashMap<>();
-		model.put("title", "Mr.");
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("title", "Mr");
 		model.put("firstName", "Matt");
-		model.put("lastName", "patel");
-		model.put("dateOfBirth", new GregorianCalendar(2006, 3, 9).getTime());
+		model.put("surname", "Greencroft");
+		model.put("dateOfBirth", new GregorianCalendar(2006,3,9).getTime());
 		model.put("description", "a <strong>fantastic</strong> Java Programmer");
 		
-		List<String> languages = new ArrayList<>();
+		List<String> languages = new ArrayList<String>();
 		languages.add("English");
 		languages.add("French");
 		languages.add("Spanish");
 		languages.add("Danish");
 		
 		model.put("languages", languages);
-		model.put("color",  "#ccc");
+		
+		model.put("color", "#ccc");
+		
 		return new ModelAndView("profile", "model", model);
 	}
 	
-	@RequestMapping("/addUser")
+	@RequestMapping("/addUser") 
 	public ModelAndView addUser() {
 		return new ModelAndView("newUser", "addUserModel" , new AddUserModel());
 	}
 	
 	@RequestMapping(value="/saveUser", method=RequestMethod.POST)
 	public String saveUser(@Valid AddUserModel addUserModel, BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
+		new AddUserModelValidator().validate(addUserModel, bindingResult);
+		if (bindingResult.hasErrors()) {
 			return "newUser";
 		}
 		return "userAdded";
